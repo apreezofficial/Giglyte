4043e29eacb8f13bdf44fa4b9fa2da5341044c11
apreezofficial <apreezofficial@gmail.com>
2025-07-09 22:30:16 +0100

    ..

--- giglyte-backend/routes/web.php
+++ giglyte-backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Models\Job;
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/users', function(){
+return Jobs::all();
 });
